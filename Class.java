5f44b47fb32e16d8b73786c256ff8ef7676a68c4
DTC-CS-Camp <68837059+DTC-CS-Camp@users.noreply.github.com>
2020-07-26 22:53:23 -0600

    Update Class.java

--- Class.java
+++ Class.java
@@ -45,26 +45,4 @@ public class Class {
 		
 		//ADD CODE ABOVE /\
 	}
-	
-	
-	public static void main(String[] args) {
-		/*PROBLEM 1
-		 *test cases for Problem 1.
-		 *when you're ready, uncomment the line below (with a regular comment), 
-		 *and then click the play button. Type a number and then hit enter. 
-		 *(remember, the number must be an integer)
-		 *once you think your program is working, call an instructor or TA to test it
-		**/
-		//Problem1();
-		
-		/*PROBLEM 2
-		 *test cases for Problem 2.
-		 *when you're ready, uncomment the line below (with a regular comment), 
-		 *and then click the play button. First, type a lower bound and then hit enter. 
-		 *Then, type an upper bound and hit enter.
-		 *(remember, the numbers must be doubles between 10 and 20)
-		 *once you think your program is working, call an instructor or TA to test it
-		**/
-		//Problem2();
-	}
 }
