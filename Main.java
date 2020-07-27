f2634acedd001793cc2f83710ccf654fdc3d28ea
DTC-CS-Camp <68837059+DTC-CS-Camp@users.noreply.github.com>
2020-07-26 22:54:09 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+  public static void main(String[] args) {
+		/*PROBLEM 1
+		 *test cases for Problem 1.
+		 *when you're ready, uncomment the line below (with a regular comment), 
+		 *and then click the play button. Type a number and then hit enter. 
+		 *(remember, the number must be an integer)
+		 *once you think your program is working, call an instructor or TA to test it
+		**/
+		//Class.Problem1();
+		
+		/*PROBLEM 2
+		 *test cases for Problem 2.
+		 *when you're ready, uncomment the line below (with a regular comment), 
+		 *and then click the play button. First, type a lower bound and then hit enter. 
+		 *Then, type an upper bound and hit enter.
+		 *(remember, the numbers must be doubles between 10 and 20)
+		 *once you think your program is working, call an instructor or TA to test it
+		**/
+		//Class.Problem2();
+    
+    /*BONUS PROBLEM
+		 *test cases for Bonus Problem.
+		 *when you're ready, uncomment the line below (with a regular comment), 
+		 *and then click the play button. 
+		**/
+		//Class.BonusProblem();
+	}
+}
